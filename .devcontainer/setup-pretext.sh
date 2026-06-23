f29d28a01060a3ba5cecea372f43bf05502c2676
eleanormcspirit <102268278+eleanormcspirit@users.noreply.github.com>
2026-06-23 22:43:10 +0000

    backend changes

--- .devcontainer/setup-pretext.sh
+++ .devcontainer/setup-pretext.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 python -m pip install --upgrade pip
-python -m pip install --user "pretext[all]"
+python -m pip install --user pretext
 
 echo ""
 echo "PreTeXt setup complete."
